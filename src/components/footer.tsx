e2cb604da5ee18bdd99ffa212043c045ac6a489b
thoraya elsharawy <thorayaelsharawy@gmail.com>
2025-04-27 10:27:10 +0300

    fix: update cv link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer className="flex items-center gap-10 text-white">
-            <Link href="https://drive.google.com/file/d/1xI0qS9_Myg073HUt3FKcmilSoVsDw5_Y/view" target="_blank">
+            <Link href="https://drive.google.com/file/d/1mW07xAWRaTBs1JYUd-KPSF6zpZt4Jfae/view?usp=sharing" target="_blank">
                 <PiReadCvLogo className="text-[25px] cursor-pointer hover:text-cyan-600 transition-all" />
             </Link>
             <Link href='https://www.linkedin.com/in/thorayaelsharawy/' target="_blank">
